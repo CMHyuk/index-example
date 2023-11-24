9ab0268b8e80e347130ae0d50fc41cf3084a0096
CMHyuk <alsgur9710@naver.com>
2023-11-24 12:59:27 +0900

    index 예제 코드

--- src/main/java/com/example/index/Init.java
+++ src/main/java/com/example/index/Init.java
@@ -12,7 +12,11 @@ public class Init {
 
     @PostConstruct
     public void saveMember() {
-        for (int i = 0; i < 200000; i++) {
+        for (int i = 0; i < 100000; i++) {
+            memberService.save("회원1");
+        }
+
+        for (int i = 0; i < 100000; i++) {
             memberService.save("회원" + i);
         }
     }
